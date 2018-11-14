a4615e514afca1d5b712490bd58fa95211a5d9ee
ghpark <ghpark@daou.co.kr>
2018-11-14 11:45:00 +0900

    test

--- app/src/main/java/com/example/ghpark/hello/MainActivity.kt
+++ app/src/main/java/com/example/ghpark/hello/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.ghpark.hello
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testView.text = "안녕1"
+
+
+        /*val myTextView = findViewById(R.id.testView) as TextView
+        myTextView.text = "Hello World!"*/
     }
 }
